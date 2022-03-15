ca5e387c0739bd7d6ae1677d7d0c93a443212d38
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-15 22:17:39 +1000

    Commence Home page interface structure

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Row, Col, Button } from 'react-bootstrap'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Row>
+      <Col md={6} className="d-flex flex-direction-column align-items-center justify-content-center">
+        <div>
+          <h1>Share the world with your friends</h1>
+          <p>Chat App lets you connect with the world</p>
+          <LinkContainer to="/chat">
+            <Button variant="success">GEt Started</Button>
+            <i className='fas fa-comments home message-icon'></i>
+          </LinkContainer>
+        </div>
+      </Col>
+    </Row>
   )
 }
 
