de94b16de69ff74d16928c6a8ab43429a255e262
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-15 21:28:50 +1000

    relocate server file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
+// initialize express app
 const express = require('express')
+// instantiate express app
 const app = express()
 
+// create chat rooms
 const rooms = ['general', 'tech', 'finance', 'crypto'];
+// require cors 
 const cors = require('cors');
 
+// pass data as urlEncoded & JSON data 
 app.use(express.urlencoded({extended: ture}));
 app.use(express.json());
+// cors allows the front & backend to communicate 
 app.use(cors());
+
 
 const server = require('http').createServer(app);
 const PORT = 5001;
@@ -20,4 +27,4 @@ const io = require('socket.io')(server, {
 
 server.listen(PORT, () => {
     console.log('ITS ALIVE!!!' , PORT)
-})
+}) 
