dcbe3fea34951bbcac711dcd2c03d1d4eea433bd
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-15 22:06:43 +1000

    Create routes to all pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Chat from './pages/Chat'
 
 function App() {
   return (
@@ -7,9 +11,9 @@ function App() {
       <Navigation/>
         <Routes>
           <Route path="/" element={<Home/>}/>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/" element={<Signup/>}/>
-          <Route path="/" element={<Chat/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+          <Route path="/chat" element={<Chat/>}/>
       </Routes>
     </BrowserRouter>
   );
