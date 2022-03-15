923ba978641ed25926cc9549aba6619df71acd12
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-03-15 21:56:38 +1000

    import routes & navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navigation from './components/Navigation'
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+      <Navigation/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/" element={<Signup/>}/>
+          <Route path="/" element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
